db5ad5904f856afa01b4426e159a8f8e1ab70a83
Kristoph Cichocki <github.com@kristoph.net>
2024-11-05 20:59:23 -0800

    - initial tests for scrape request

---
+++ spec/scrape_request_spec.rb
@@ -0,0 +1,186 @@
+require 'spec_helper'
+
+RSpec.describe Firecrawl::ScrapeRequest do
+
+  include_context 'vcr'
+
+  before do
+    raise "A FIRECRAWL_API_KEY must be defined in the environment." unless ENV[ 'FIRECRAWL_API_KEY' ]
+  end
+
+  let ( :url ) {
+    "https://www.iana.org/help/example-domains"
+  }
+
+  let ( :scrape_request ) {
+    described_class.new( connection: connection, api_key: ENV[ 'FIRECRAWL_API_KEY' ] )
+  }
+
+  context "where there are no scrape options" do 
+    it "scrapes the given url, returning main content in markdown" do 
+      response = scrape_request.scrape( url )
+
+      expect( response ).to be_a( Faraday::Response )
+      expect( response.success? ).to be true
+      expect( response.result ).to be_a( Firecrawl::ScrapeResult )
+
+      result = response.result 
+      expect( result.success? ).to be true   
+      expect( result.metadata[ 'title' ] ).to match( /example domains/i )
+      # markdown is the default
+      expect( result.markdown ).to_not be_nil
+      expect( result.markdown ).to match( /number of domains such as example/ )
+      # main content only is the default 
+      expect( result.markdown ).not_to match( /about/i )
+
+      expect( result.html ).to be_nil
+      expect( result.raw_html ).to be_nil
+      expect( result.links ).to eq( [] )
+      expect( result.screenshot_url ).to be_nil
+      expect( result.actions ).to eq( {} )
+      expect( result.llm_extraction ).to eq( {} )
+      expect( result.warning ).to be_nil
+    end 
+  end
+
+  context "where the scrape options include 'only_main_content false'" do 
+
+    let ( :options ) {
+      Firecrawl::ScrapeOptions.build do 
+        only_main_content false 
+      end 
+    }
+
+    it "scrapes the given url returning all content in markdown" do 
+      response = scrape_request.scrape( url, options )
+
+      expect( response ).to be_a( Faraday::Response )
+      expect( response.success? ).to be true
+      expect( response.result ).to be_a( Firecrawl::ScrapeResult )
+
+      result = response.result 
+      expect( result.success? ).to be true   
+      expect( result.metadata[ 'title' ] ).to match( /example domains/i )
+      # markdown is the default
+      expect( result.markdown ).to_not be_nil
+      expect( result.markdown ).to match( /number of domains such as example/i )
+      expect( result.markdown ).to match( /about/i )
+
+      expect( result.html ).to be_nil
+      expect( result.raw_html ).to be_nil
+      expect( result.links ).to eq( [] )
+      expect( result.screenshot_url ).to be_nil
+      expect( result.actions ).to eq( {} )
+      expect( result.llm_extraction ).to eq( {} )
+      expect( result.warning ).to be_nil
+    end 
+  end
+
+  context "where the scrape options include 'only_main_content true'" do 
+
+    let ( :options ) {
+      Firecrawl::ScrapeOptions.build do 
+        only_main_content true 
+      end 
+    }
+
+    it "scrapes the given url, returning main content in markdown" do 
+      response = scrape_request.scrape( url )
+
+      expect( response ).to be_a( Faraday::Response )
+      expect( response.success? ).to be true
+      expect( response.result ).to be_a( Firecrawl::ScrapeResult )
+
+      result = response.result 
+      expect( result.success? ).to be true   
+      expect( result.metadata[ 'title' ] ).to match( /example domains/i )
+      # markdown is the default
+      expect( result.markdown ).to_not be_nil
+      expect( result.markdown ).to match( /number of domains such as example/i )
+      # main content only is the default 
+      expect( result.markdown ).not_to match( /about/i )
+
+      expect( result.html ).to be_nil
+      expect( result.raw_html ).to be_nil
+      expect( result.links ).to eq( [] )
+      expect( result.screenshot_url ).to be_nil
+      expect( result.actions ).to eq( {} )
+      expect( result.llm_extraction ).to eq( {} )
+      expect( result.warning ).to be_nil
+    end 
+  end
+
+  context "where the scrape options include 'format :html'" do 
+
+    context "where the scrape options include 'only_main_content false'" do 
+
+      let ( :options ) {
+        Firecrawl::ScrapeOptions.build do 
+          format :html
+          only_main_content false 
+        end 
+      }
+
+      it "scrapes the given url, returning all content in html" do 
+        response = scrape_request.scrape( url, options )
+
+        expect( response ).to be_a( Faraday::Response )
+        expect( response.success? ).to be true
+        expect( response.result ).to be_a( Firecrawl::ScrapeResult )
+
+        result = response.result 
+        expect( result.success? ).to be true   
+        expect( result.metadata[ 'title' ] ).to match( /example domains/i )
+        expect( result.html ).to_not be_nil
+        expect( result.html ).to match( /number of domains such as example/i )
+        expect( result.html ).to match( /about/i )
+
+        expect( result.markdown ).to be_nil
+        # expect( result.html ).to be_nil
+        expect( result.raw_html ).to be_nil
+        expect( result.links ).to eq( [] )
+        expect( result.screenshot_url ).to be_nil
+        expect( result.actions ).to eq( {} )
+        expect( result.llm_extraction ).to eq( {} )
+        expect( result.warning ).to be_nil
+      end 
+    end
+
+    context "where the scrape options include 'only_main_content true'" do 
+
+      let ( :options ) {
+        Firecrawl::ScrapeOptions.build do 
+          formats :html
+          only_main_content true 
+        end 
+      }
+
+      it "scrapes the given url, returning main content in html" do 
+        response = scrape_request.scrape( url, options )
+
+        expect( response ).to be_a( Faraday::Response )
+        expect( response.success? ).to be true
+        expect( response.result ).to be_a( Firecrawl::ScrapeResult )
+
+        result = response.result 
+        expect( result.success? ).to be true   
+        expect( result.metadata[ 'title' ] ).to match( /example domains/i )
+        expect( result.html ).to_not be_nil
+        expect( result.html ).to match( /number of domains such as example/i )
+        expect( result.html ).not_to match( /about/i )
+
+        expect( result.markdown ).to be_nil
+        # expect( result.html ).to be_nil
+        expect( result.raw_html ).to be_nil
+        expect( result.links ).to eq( [] )
+        expect( result.screenshot_url ).to be_nil
+        expect( result.actions ).to eq( {} )
+        expect( result.llm_extraction ).to eq( {} )
+        expect( result.warning ).to be_nil
+      end 
+    end
+
+  end
+
+end 
+
